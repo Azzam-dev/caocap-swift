741bbd5d8c2119c87ad460a08ff06e77219b0c1b
Azzam <azzam.rar@gmail.com>
2020-05-07 07:54:17 +0300

    deleted cosmosVC.swift, orbitVC.swift , cosmosCaocapCell.swift , cosmosCell.swift & worked on BuilderVC and added ArtBuilderVC

--- caocap/ViewControllers/BuilderVCs/ArtBuilderVC.swift
+++ caocap/ViewControllers/BuilderVCs/ArtBuilderVC.swift
@@ -1,15 +1,15 @@
 //
-//  BuilderVC.swift
+//  ArtBuilderVC.swift
 //  caocap
 //
-//  Created by Azzam AL-Rashed on 09/12/2019.
-//  Copyright © 2019 Ficruty. All rights reserved.
+//  Created by Azzam AL-Rashed on 07/05/2020.
+//  Copyright © 2020 Ficruty. All rights reserved.
 //
 
 import UIKit
 
-class BuilderVC: UIViewController {
-    
+class ArtBuilderVC: UIViewController {
+
     var blocksArray = [ #imageLiteral(resourceName: "icons8-accounting0"), #imageLiteral(resourceName: "icons8-trash0"), #imageLiteral(resourceName: "icons8-lightning_bolt0"), #imageLiteral(resourceName: "icons8-location_off_filled0"), #imageLiteral(resourceName: "icons8-geo_fence0"), #imageLiteral(resourceName: "W-search_filled"), #imageLiteral(resourceName: "icons8-menu0"), #imageLiteral(resourceName: "W-full_screen_filled"), #imageLiteral(resourceName: "W-uncheck_all_filled"), #imageLiteral(resourceName: "icons8-paper_plane_filled0"), #imageLiteral(resourceName: "icons8-no_microphone0"), #imageLiteral(resourceName: "icons8-galaxy0"), #imageLiteral(resourceName: "W-network"), #imageLiteral(resourceName: "icons8-astronaut_helmet0"), #imageLiteral(resourceName: "w-share"), #imageLiteral(resourceName: "W-full_screen_filled"), #imageLiteral(resourceName: "W-uncheck_all_filled"), #imageLiteral(resourceName: "icons8-paper_plane_filled0"), #imageLiteral(resourceName: "icons8-no_microphone0"), #imageLiteral(resourceName: "icons8-galaxy0")]
     
     @IBOutlet weak var designScrollView: UIScrollView!
@@ -25,16 +25,16 @@ class BuilderVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+       setupViews()
+    }
+    
+    func setupViews() {
         designScrollView.contentSize = CGSize(width: viewWidth * 2, height: viewHeight * 2)
         designScrollView.contentOffset = CGPoint(x: viewWidth / 2 , y: viewHeight / 2 )
         
         designScrollView.addSubview(designSVContant)
         designSVContant.frame.size = designScrollView.contentSize
-        
-//        if let surface1 = Bundle.main.loadNibNamed("SurfaceBlock", owner: self, options: nil)?.first as? SurfaceBlock {
-//            designScrollView.addSubview(surface1)
-//        }
         
         designSVContant.addSubview(surfaceBlock)
         surfaceBlock.frame.origin = CGPoint(x: viewWidth * 0.75, y: viewHeight * 0.6)
@@ -45,12 +45,12 @@ class BuilderVC: UIViewController {
         
         designSVContant.addSubview(surfaceBlock3)
         surfaceBlock3.frame.origin = CGPoint(x: surfaceBlock.frame.origin.x - 120 , y: surfaceBlock.frame.origin.y + 300)
-        
     }
 }
 
 
-extension BuilderVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+extension ArtBuilderVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -68,7 +68,7 @@ extension BuilderVC: UICollectionViewDelegate, UICollectionViewDataSource, UICol
     
 }
 
-extension BuilderVC: UIScrollViewDelegate {
+extension ArtBuilderVC: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return designSVContant
     }
---
+++ caocap/ViewControllers/BuilderVCs/BuilderVC.swift
@@ -0,0 +1,29 @@
+//
+//  BuilderVC.swift
+//  caocap
+//
+//  Created by Azzam AL-Rashed on 09/12/2019.
+//  Copyright © 2019 Ficruty. All rights reserved.
+//
+
+import UIKit
+
+class BuilderVC: UIViewController {
+    
+    lazy var viewWidth = self.view.frame.width
+    lazy var viewHeight = self.view.frame.height
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        
+    }
+    
+    func setupViews() {
+        //TODO: add ArtBuilderVC & LogicMindMapVC & CosmosBaseVC & labVC
+    }
+    
+}
+
+
