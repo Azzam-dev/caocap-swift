79a003a36d8d43094997c8b53c6d8e7d847534d2
Azzam <azzam.rar@gmail.com>
2020-05-16 06:58:32 +0300

    worked on logicScrollView & added classBlock

--- caocap/ViewControllers/BuilderVCs/LogicMindMapVC.swift
+++ caocap/ViewControllers/BuilderVCs/LogicMindMapVC.swift
@@ -9,14 +9,32 @@
 import UIKit
 
 class LogicMindMapVC: UIViewController {
-
- 
+    
+    
     @IBOutlet weak var toolsViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var gestureRecognizerView: UIView!
     var topToolBarSelectedIndex: Int = 2
     
     @IBOutlet weak var selectorView: DesignableView!
     @IBOutlet var topToolBarBTNs: [UIButton]!
+    
+    @IBOutlet weak var logicScrollView: UIScrollView!
+    @IBOutlet weak var logicSVContant: UIView!
+    @IBOutlet var classBlock: UIStackView!
+    @IBOutlet var classBlock2: UIStackView!
+    @IBOutlet var classBlock3: UIStackView!
+    
+    lazy var viewWidth = self.view.frame.width
+    lazy var viewHeight = self.view.frame.height
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        gestureRecognizerSetup()
+    }
+    
+    
     @IBAction func topToolBarBTNs(_ sender: UIButton) {
         
         topToolBarSelectedIndex = sender.tag
@@ -29,18 +47,25 @@ class LogicMindMapVC: UIViewController {
             UIView.animate(withDuration: 0.1) {
                 self.selectorView.shadowOpacity = 0.2
             }
-            
         })
-        
-       
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    
-
-        gestureRecognizerSetup()
+    func setupViews() {
+        logicScrollView.contentSize = CGSize(width: viewWidth * 2, height: viewHeight * 2)
+        logicScrollView.contentOffset = CGPoint(x: viewWidth / 2 , y: viewHeight / 2 )
+        
+        logicScrollView.addSubview(logicSVContant)
+        logicSVContant.frame.size = logicScrollView.contentSize
+        
+        logicSVContant.addSubview(classBlock)
+        classBlock.frame.origin = CGPoint(x: viewWidth * 0.75, y: viewHeight * 0.55)
+        
+        logicSVContant.addSubview(classBlock2)
+        classBlock2.frame.origin = CGPoint(x: classBlock.frame.origin.x + 220 , y: viewHeight * 0.55)
+        
+        
+        logicSVContant.addSubview(classBlock3)
+        classBlock3.frame.origin = CGPoint(x: classBlock.frame.origin.x - 220 , y: viewHeight * 0.55)
     }
     
     func gestureRecognizerSetup() {
@@ -78,10 +103,19 @@ class LogicMindMapVC: UIViewController {
     }
     
     func toolsViewAnimation(_ hight: Int) {
-           UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 3, options: .curveLinear, animations: {
-               self.toolsViewHeightConstraint.constant = CGFloat(hight)
-               self.view.layoutIfNeeded()
-           })
-       }
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 3, options: .curveLinear, animations: {
+            self.toolsViewHeightConstraint.constant = CGFloat(hight)
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+}
 
+extension LogicMindMapVC: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return logicSVContant
+    }
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        
+    }
 }
