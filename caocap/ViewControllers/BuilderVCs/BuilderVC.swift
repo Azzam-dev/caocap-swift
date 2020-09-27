72d562e6d448bf908e187d3bc5344b8dee5308f1
Azzam <azzam.rar@gmail.com>
2020-09-27 12:04:26 +0300

    worked on builder VCs

--- caocap/ViewControllers/BuilderVCs/BuilderVC.swift
+++ caocap/ViewControllers/BuilderVCs/BuilderVC.swift
@@ -10,34 +10,66 @@ import UIKit
 
 class BuilderVC: UIViewController {
     
+    var artNAV: UINavigationController!
+    var logicNAV: UINavigationController!
+    var cosmosNAV: UINavigationController!
+    var testLabNAV: UINavigationController!
     
     @IBOutlet weak var contentView: UIView!
+    
     var openedCaocap = Caocap(key: "", dictionary: ["":""])
     
     var viewControllers: [UIViewController]!
-    var navSelectedIndex: Int = 0
+    var navSelectedIndex: Int = 3
+    
+    lazy var viewWidth = self.view.frame.width
+    lazy var viewHeight = self.view.frame.height
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.hideKeyboardWhenTappedAround()
-        setupSubViewControllers()
+        setupSubNavigationControllers()
     }
     
     @IBAction func exitBuilder(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    func setupSubViewControllers() {
+    func setupSubNavigationControllers() {
+        
         let storyboard = UIStoryboard(name: "Builder", bundle: nil)
-        let testLabVC = storyboard.instantiateViewController(withIdentifier: "testLab") as? TestLabVC
-        testLabVC?.openedCaocap = openedCaocap
-        viewControllers = [testLabVC!]
-        let vc = viewControllers[0]
+        artNAV = storyboard.instantiateViewController(withIdentifier: "artBuilderNAV") as? UINavigationController
+        logicNAV = storyboard.instantiateViewController(withIdentifier: "logicMindMapNAV") as? UINavigationController
+        cosmosNAV = storyboard.instantiateViewController(withIdentifier: "cosmosBaseNAV") as? UINavigationController
+        testLabNAV = storyboard.instantiateViewController(withIdentifier: "testLapNAV") as? UINavigationController
+        navigationControllers = [cosmosNAV, logicNAV,artNAV, testLabNAV]
+        navBTNpressed(navBTNs[navSelectedIndex])
+    }
+    
+    @IBOutlet weak var selectorView: DesignableView!
+    @IBOutlet var navBTNs: [UIButton]!
+    @IBAction func navBTNpressed(_ sender: UIButton) {
+        
+        let previousNavIndex = navSelectedIndex
+        navSelectedIndex = sender.tag
+        
+        let previousVC = navigationControllers[previousNavIndex]
+        previousVC.willMove(toParent: nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParent()
+        
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 3, options: .curveLinear, animations: {
+            self.selectorView.frame.origin.x = self.navBTNs[self.navSelectedIndex].frame.origin.x - 10
+        })
+        
+        
+        let vc = navigationControllers[navSelectedIndex]
         addChild(vc)
         vc.view.frame = contentView.bounds
         contentView.addSubview(vc.view)
         vc.didMove(toParent: self)
+        
     }
     
 }
