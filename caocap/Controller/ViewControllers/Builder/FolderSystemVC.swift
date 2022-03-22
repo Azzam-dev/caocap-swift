c8a8ef4c2b4108d1f19754e8266c72eb0f4073cb
Azzam R Alrashed <Azzam.rar@gmail.com>
2022-03-23 02:05:41 +0300

    setup FolderSystemVC and some work on the logic mind map ui design in ArtBuilderVC

---
+++ caocap/Controller/ViewControllers/Builder/FolderSystemVC.swift
@@ -0,0 +1,19 @@
+//
+//  FolderSystemVC.swift
+//  caocap
+//
+//  Created by CAOCAP inc on 20/08/1443 AH.
+//  Copyright © 1443 Ficruty. All rights reserved.
+//
+
+import UIKit
+
+class FolderSystemVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
